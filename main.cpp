398837f90af559fbb0b4f92af5b7ca818619e3b0
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 21:51:21 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+	int x;
+	cout << "¬вед≥ть число, щоб перев≥рити його: ";
+	cin >> x;
+	if (x > 0) {
+		cout << "„исло додатне";
+	}
+	else if (x < 0) {
+		cout << "„исло в≥д'Їмне";
+	}
+	else {
+		cout << "„исло дор≥внюЇ нулю";
+	}
 
     system("pause");
     return 0;
