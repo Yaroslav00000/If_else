0e8c6837704adfdf9e82e34c305401ad441e1b13
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 21:49:54 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+	int x;
+	cout << "¬вед≥ть число, щоб перев≥рити його на парн≥сть: ";
+	cin >> x;
+	if (x % 2 == 0)
+	{
+		cout << "„исло парне";
+	}
     system("pause");
     return 0;
 }
