82a0fb7780c682be521323f839faba450808e38c
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-11 21:50:50 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+	int x;
+	int two;
+	cout << "¬вед≥ть перше число: ";
+	cin >> x;
+	cout << "¬вед≥ть друге число: ";
+	cin >> two;
+	if (x > two) {
+		cout << two;
+	}
+	else {
+		cout << x;
+	}
+	cout << endl;
 
     system("pause");
     return 0;
